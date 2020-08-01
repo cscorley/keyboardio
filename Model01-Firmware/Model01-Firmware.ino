73d066336be84dcef48b9dfd0535d95c78af117e
Christopher S. Corley <cscorley@gmail.com>
2020-07-31 21:42:25 -0400

    so apparently without eeprom/colormap layers you can enter an error state

--- Model01-Firmware/Model01-Firmware.ino
+++ Model01-Firmware/Model01-Firmware.ino
@@ -422,12 +422,12 @@ void setup()
   // one wants to use these layers, just set the default layer to one in EEPROM,
   // by using the `settings.defaultLayer` Focus command, or by using the
   // `keymap.onlyCustom` command to use EEPROM layers only.
-  EEPROMKeymap.setup(0);
+  EEPROMKeymap.setup(5);
 
   // We need to tell the Colormap plugin how many layers we want to have custom
   // maps for. To make things simple, we set it to five layers, which is how
   // many editable layers we have (see above).
-  ColormapEffect.max_layers(0);
+  ColormapEffect.max_layers(5);
 
   MouseKeys.speed = 12;
   MouseKeys.speedDelay = 1;
