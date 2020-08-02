9bbdde1e1d260f59f30a68b6e85c43d8e7c528db
Christopher S. Corley <cscorley@gmail.com>
2020-08-01 21:55:08 -0400

    swap brackets

--- Model01-Firmware/Model01-Firmware.ino
+++ Model01-Firmware/Model01-Firmware.ino
@@ -173,16 +173,16 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,            Key_F2,        Key_F3,          Key_F4,           Key_F5, Key_CapsLock,
-   Key_Tab,  ___,               Key_mouseUp,   Key_LeftBracket, Key_RightBracket, ___,    Key_mouseScrollUp,
-   Key_Home, Key_mouseL,        Key_mouseDn,   Key_mouseR,      Key_mouseBtnL,    Key_mouseBtnR,
-   Key_End,  ___,               Key_Insert,    ___,             Key_mouseBtnM,    ___,    Key_mouseScrollDn,
+  (___,      Key_F1,            Key_F2,        Key_F3,          Key_F4,               Key_F5, Key_CapsLock,
+   Key_Tab,  ___,               Key_mouseUp,   Key_LeftBracket, Key_LeftCurlyBracket, ___,    Key_mouseScrollUp,
+   Key_Home, Key_mouseL,        Key_mouseDn,   Key_mouseR,      Key_mouseBtnL,        Key_mouseBtnR,
+   Key_End,  ___,               Key_Insert,    ___,             Key_mouseBtnM,        ___,    Key_mouseScrollDn,
 
    ___, Key_Delete, ___, ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_RightCurlyBracket,    Key_RightBracket,         Key_LeftBracket, Key_RightBracket, Key_F12,
                                Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
 
    Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
